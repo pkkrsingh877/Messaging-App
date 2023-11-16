d4c19a795ccfef2297d8c147352ea65a430d9f61
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-11-16 06:38:30 +0530

    Creating Friend System

--- models/user.js
+++ models/user.js
@@ -2,7 +2,19 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User
+    }],
+    friendRequestSent: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User
+    }],
+    friendRequestReceived: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User
+    }]
 });
 
 const User = mongoose.model('User', userSchema);
