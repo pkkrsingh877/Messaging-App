51ac6ddb026547605b81a0bce459761173c15e76
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-11-28 14:32:30 +0530

    Code for delete and update posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -16,12 +16,29 @@ const getCreatePost = () => {
 
 }
 
-const updatePost = () => {
+const updatePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, description, user, comments } = req.body;
 
+        const post = Post.findByIdAndUpdate(id, {
+            title, description, user, comments, updatedAt: Date.now()
+        });
+
+        res.status(200).json({"Status": "Success"});
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }
 
-const deletePost = () => {
-
+const deletePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Post.findByIdAndDelete(id);
+        res.status(200).json({ "Status": "Success" })
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }
 
 module.exports = { getPosts, getPost, getCreatePost, postCreatePost, deletePost, updatePost };
