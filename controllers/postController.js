c7cbe9a7bb5998880a12297aa965d354f2b232d9
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-11-30 04:25:41 +0530

    Posts page is now being served

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,19 +1,41 @@
 const Post = require('../models/post');
 
-const getPosts = () => {
-
+const getPosts = async (req, res) => {
+    try {
+        const posts = await Post.find({});
+        res.status(200).render('posts/posts', { posts });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({"Status": "Failure"});
+    }
 }
 
-const getPost = () => {
-
+const getPost = (req, res) => {
+    try {
+        const { id } = req.params;
+        res.redirect(`http://localhost:8000/post/${id}`);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({"Status": "Failure"});
+    }
 }
 
-const postCreatePost = () => {
-
+const postCreatePost = async (req, res) => {
+    try {
+        const { title, description, comments } = req.body;
+        const post = await Post.create({ title, description, comments });
+        res.redirect(`http://localhost:8000/post/${post._id}`);
+    } catch (error) {
+        res.status(400).json(error);   
+    }
 }
 
-const getCreatePost = () => {
-
+const getCreatePost = (req, res) => {
+    try {
+        res.render('posts/create');
+    } catch (error) {
+        res.json({"Status": "Ejs related error"});
+    }
 }
 
 const updatePost = async (req, res) => {
@@ -21,7 +43,7 @@ const updatePost = async (req, res) => {
         const { id } = req.params;
         const { title, description, user, comments } = req.body;
 
-        const post = Post.findByIdAndUpdate(id, {
+        const post = await Post.findByIdAndUpdate(id, {
             title, description, user, comments, updatedAt: Date.now()
         });
 
