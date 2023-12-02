6a4cd430b0249137837a181c51ec2c96944974f2
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-12-02 10:50:17 +0530

    Added page to list friends requests, routes to listen to send, accept, reject friend requests

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { getLogin, postLogin, getSignup, postSignup } = require('../controllers/authController');
 const { getSearch, postSearch, getUser } = require('../controllers/homeController');
+const { sendFriendRequest, acceptFriendRequest, rejectFriendRequest } = require('../controllers/friendSystemController');
+
+router.get('/sendFriendRequest', sendFriendRequest);
+
+router.get('/acceptFriendRequest', acceptFriendRequest);
+
+router.get('/rejectFriendRequest', rejectFriendRequest);
 
 router.post('/search', postSearch);
 
