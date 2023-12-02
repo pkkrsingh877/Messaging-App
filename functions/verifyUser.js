1d5a5e93d2a3d203c7185065a266b9cf0c1d0609
Prabhat Kumar <pkkrsingh877@gmail.com>
2023-12-02 12:04:37 +0530

    Sending user to login page if they logout and try to access posts route

--- functions/verifyUser.js
+++ functions/verifyUser.js
@@ -11,13 +11,14 @@ const verifyUser = async (req, res, next) => {
         if(token){
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = decoded.userId
+        }else{
+            throw new Error('User not logged in!');
         }
 
         // Let the request continue by calling next() function
         next();
     } catch (error) {
-        console.log(error);
-        
+        res.status(301).redirect('http://localhost:8000/auth/login');
     }
 }
 
